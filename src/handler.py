1c913b1796978d9e3c68058c3b053696a2428ce7
nic-gunter-gtchs <nicolas.gunter@student.gtchs.org>
2025-09-29 16:44:43 -0400

    Update handler.py

--- src/handler.py
+++ src/handler.py
@@ -1,1 +1,8 @@
 # a module for all of the major program processing, including all interactions with the DB
+import sqlite3, json
+
+#TODO: write function for reading sqlite db and returning the data efficiently
+
+#TODO: write function for updating sqlite db (check for malicious or incorrect requests)
+
+#TODO: write function for reading/writing favorites json
