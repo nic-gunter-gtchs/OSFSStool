cd83ef27a236b90e52552068cffc925756b793af
nic-gunter-gtchs <nicolas.gunter@student.gtchs.org>
2025-09-29 17:37:28 -0400

    Update handler.py

--- src/handler.py
+++ src/handler.py
@@ -1,6 +1,7 @@
 # a module for all of the major program processing, including all interactions with the DB
 import sqlite3, json
-
+con = sqlite3.connect("data/osfss.db")
+cur = con.cursor()
 #TODO: write function for reading sqlite db and returning the data efficiently
 
 #TODO: write function for updating sqlite db (check for malicious or incorrect requests)
