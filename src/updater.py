c57045dbd6a682c5ec758fbe4080aeaf7e0da7eb
nic-gunter-gtchs <nicolas.gunter@student.gtchs.org>
2025-09-29 17:08:40 -0400

    Update updater.py

--- src/updater.py
+++ src/updater.py
@@ -1,1 +1,12 @@
 # CLI app that provides a frontend to add definitions and article links to the DB
+from handler import *
+
+print("OSFSSdb Updater\n")
+subject = input("Enter the acronym you'd like to update the information for: ").upper().strip()
+#TODO: call db read function and check for existing values. if so, expect output formatted as {"titles":[],"descs":[],"links":[]} where a blank list element is used if no value is provided in each category
+
+if result != None:
+  print(f"\nAcronym {subject} has existing data:")
+  for title in range(result["titles"]):
+    print(f"{result["titles"][title]}: {result["descs"][title]}\n{result["links"][title]}\n")
+  ntitle = input("Please enter the definition you'd like to update or create. ").capitalize()
